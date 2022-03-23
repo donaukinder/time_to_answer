3585da1831e30f87146558f7aab046ed837c93a6
donaukinder <higor.carvalho94@gmail.com>
2022-03-22 22:50:20 -0400

    142 e 143

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js )
+
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css custom.css custom.js )
+
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
